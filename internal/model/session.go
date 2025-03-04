130af2975a3d1a6aae5ca9278e1a670199ed197d
mia <viashipcosmos@gmail.com>
2025-03-05 00:58:59 +0800

    clear

--- internal/model/session.go
+++ internal/model/session.go
@@ -1,1 +1,10 @@
 package model
+
+import "time"
+
+type Session struct {
+	ID        uint      `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	BelongTo  uint      `json:"belong_to"`
+}
